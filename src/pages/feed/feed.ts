04f2368ef9ab39d55a37cc4c9a2ed2dcd6910b68
arllain <arllain@gmail.com>
2018-01-09 00:30:40 -0300

    third commit

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,20 +13,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
-  public nomeUsuario:string = "Arllain Candido"
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public objeto_feed = {
+    titulo: "Arllain Candido",
+    data: "08/01/2018",
+    descricao: "My first ionic app",
+    qtdn_likes: 24,
+    qtdn_comments: 200,
+    time_comment: "08 jandeiro de 2018"
   }
 
-  public somaDoisNumeros(num1:number,num2:number): void{
-    alert(num1+num2);
+  public lista_filmes = new Array<any>();
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private movieProvider: MovieProvider) {
+  }
+
+  public somaDoisNumeros(num1: number, num2: number): void {
+    alert(num1 + num2);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
-   // this.somaDoisNumeros(41,1976);
+    this.movieProvider.getLatestMovies().subscribe(
+      data => {
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+        console.log(objeto_retorno);
+      }, error => {
+        console.log(error);
+      }
+
+    );
   }
 
 }
